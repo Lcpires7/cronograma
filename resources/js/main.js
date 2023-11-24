0fa78bccdcdde944dbb95578895155fd47669bc5
Lucas Pires <lcpires7.sophia@gmail.com>
2023-11-23 21:53:11 -0300

    Update arquivos

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,3 @@
-console.log("Init")
-
-for (var i = 0; i < 9; i++) {
-  console.log(i);
-  // more statements
+  for (let i = 1; i <= 50; i++) {
+	console.log(`chapter${i}: {acf: 'texto from chapter ${i} \n'}`)
 };
